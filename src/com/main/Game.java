d195354d79175e8392919e3a30c82ec57ddd9925
Your Nameevandrolucas60 <evandrolucas60@yahoo.com>
2022-12-22 12:20:24 -0300

    Implementing the Window class and Game Class

---
+++ src/com/main/Game.java
@@ -0,0 +1,19 @@
+package com.main;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
+	private static final long serialVersionUID = -668240625892092763L;
+	
+	public Game() {
+		
+	}
+
+	@Override
+	public void run() {
+	}
+	
+	public void start() {
+	}
+
+}
